fb83cabd361ba74104141523a356690ffb23c08f
Arseny Korkodinov <senchaaa3@gmail.com>
2023-05-20 12:23:29 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            TimeSpan stw = beginWorkingTime;
+
+            List<string> periods = new List<string>();
+
+            while (stw.Add(TimeSpan.FromMinutes(consultationTime)) < endWorkingTime)
+            {
+                for (int i = 0; i < startTimes.Length; i++)
+                {
+                    if (stw != startTimes[i])
+                    {
+                        periods.Add(stw + "-" + stw.Add(TimeSpan.FromMinutes(consultationTime)));
+                    }
+                    else
+                    {
+                        TimeSpan busy = startTimes[i] + TimeSpan.FromMinutes(durations[i]);
+                        stw.Add(busy);
+                    }
+                }
+
+
+                stw.Add(TimeSpan.FromMinutes(consultationTime));
+            }
+            
+            return periods.ToArray();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1;
+Console.WriteLine("adawda");
+Class1 calculations = new Class1();
+TimeSpan[] startTimes = new TimeSpan[]
+{
+                new TimeSpan(10,0,0),
+                new TimeSpan(11,0,0),
+                new TimeSpan(15, 0,0),
+                new TimeSpan(15,30,0),
+                new TimeSpan(16,50,0),
+};
+int[] durations = new int[] { 60, 30, 10, 10, 40 };
+TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+TimeSpan endWorkingTime = new TimeSpan(18, 0, 0);
+int consulationTime = 30;
+var actual = calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consulationTime);
+Console.WriteLine(actual);
